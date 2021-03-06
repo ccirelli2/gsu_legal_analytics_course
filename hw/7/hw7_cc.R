f4196c79510833601b5f92212c7427062bc7f9eb
Born2Ride <30387055+ccirelli2@users.noreply.github.com>
2021-03-06 09:59:40 -0500

    hw7

--- hw/7/hw7_cc.R
+++ hw/7/hw7_cc.R
@@ -29,6 +29,7 @@ rm(list=ls())
 library(quanteda) 
 library(readtext) 
 library(tidyverse) 
+library(ggplot2)
 
 ################################################################################
 # Load Data
@@ -66,29 +67,103 @@ df.corp.seg <- cbind(text=texts(corp.seg),
                      stringsAsFactors=FALSE)
 
 # Write to csv
-setwd("C:\\Users\\chris.cirelli\\Desktop\\repositories\\gsu_legal_analytics_course\\hw\\7")
-write_csv(as.data.frame(df.corp.seg), 'hw7_segmented_txt_presiding.csv')
-head(df.corp.seg)
-
+#setwd("C:\\Users\\chris.cirelli\\Desktop\\repositories\\gsu_legal_analytics_course\\hw\\7")
+#write_csv(as.data.frame(df.corp.seg), 'hw7_segmented_txt_presiding.csv')
+#head(df.corp.seg)
 
 ################################################################################
 # 2: Merge Member Info
 ################################################################################
 
 # Inspect Speaker Names
+head(member.info)
 member.info$speaker[1:10]
 member.info$firstname[1:10]
 member.info$speaker[1:10]
 member.info.repeat_ln <- subset(member.info, repeat_last_name == "x")
 member.info.repeat_ln$speaker
 
+# Convert Member Info Names to Lowercase
+member.info$speaker <- tolower(member.info$speaker)
 
 # Get Last Name From Crec Corpus
-df.corp.seg$lastname <- str_remove(str_extract(df.corp.seg$pattern, "\\.*[A-Z]+\\."), "\\.")
+df.corp.seg$lastname <- tolower(str_remove(str_extract(df.corp.seg$pattern, "\\.*[A-Z]+\\."), "\\."))
 
-# Left Merge Speaker Data On Speaker Last Name
+# Test Equality
+df.corp.seg$lastname[1]
+member.info$speaker[360]
+df.corp.seg$lastname[1] == member.info$speaker[360]
+
+# Left Join Speaker Data On Speaker Last Name
+df.corp.seg.mem.info.all <- merge(x=df.corp.seg, y=member.info, by.x="lastname", by.y="speaker", all.x=TRUE)
+df.corp.seg.mem.info.lim <- merge(x=df.corp.seg, y=member.info, by.x="lastname", by.y="speaker")
+head(df.corp.seg.mem.info.lim)
+
+# Write to CSV
+setwd("C:\\Users\\chris.cirelli\\Desktop\\repositories\\gsu_legal_analytics_course\\hw\\7")
+write_csv(as.data.frame(df.corp.seg.mem.info.lim), 'hw7_segmented_txt_speaker_plus_info.csv')
 
 
+
+################################################################################
+# 4: Text Comparison Method - 
+################################################################################
+
+# Create Corpus
+df.txt.party <- df.corp.seg.mem.info.lim[c("text.x", "party")]
+corp.txt.party <- corpus(df.txt.party, text="text.x")
+
+# Create Dfm & Group By Party
+dfm.txt.party <- dfm(corp.txt.party,
+                     groups='party',
+                     remove = stopwords("en"),
+                     remove_punct=TRUE,
+                     remove_numbers=TRUE,
+                     remove_symbols=TRUE)
+
+# Write to working directory
+View(dfm.txt.party)
+setwd("C:\\Users\\chris.cirelli\\Desktop\\repositories\\gsu_legal_analytics_course\\hw\\7")
+write_csv(as.data.frame(dfm.txt.party), 'hw7_segmented_dfm_txt_by_party.csv')
+
+# Get Top 20 Tokens 
+tstat_party <- textstat_frequency(dfm.txt.party, n = 20, groups = "party")
+
+# Plot
+ggplot(data = tstat_party, aes(x = factor(nrow(tstat_party):1), y = frequency)) +
+  geom_point() +
+  facet_wrap(~ group, scales = "free") +
+  coord_flip() +
+  scale_x_discrete(breaks = nrow(tstat_party):1,
+                   labels = tstat_party$feature) +
+  labs(x = NULL, y = "Relative Frequency by Party")
+
+# Word Cloud
+dev.new(width = 1000, height = 1000, unit = "px")
+textplot_wordcloud(dfm.txt.party, comparison = TRUE)
+
+# Text Similarity
+tstat_cosine <- textstat_simil(dfm.txt.party, method = "cosine", margin = "documents")
+tstat_cosine_df <- as.data.frame(tstat_cosine)
+View(tstat_cosine_df)
+'result = 0.678'
+
+
+
+
+################################################################################
+# 5. Comments on Text Similarity
+################################################################################
+'
+ Top 20 tokens & Word Cloud Comparis:
+ - A number of similar tokens are found in the top 20 tokens utilized by both
+   republicans and demographs.  These include senate, president, states,
+   health.
+ - That said, many more do not.  Examples include district, care, new, secretary,
+   united, trump.
+ - Simply based on the vector of the words used in the dfm the resulting cosine
+   similarity was 0.678, which indicates that they are fairly similar text.
+'
 
 
 
